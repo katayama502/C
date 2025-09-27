8bdd4e5154ceb0e9d9a1d25b4b4681eb07f909f0
katayama hayato <>
2025-09-27 12:09:18 +0900

    調整

--- crietto/app/Console/Kernel.php
+++ crietto/app/Console/Kernel.php
@@ -7,10 +7,11 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    protected function schedule(Schedule $schedule): void
+    protected function schedule(Schedule $schedule)
     {
-        //
+        $schedule->command('inspire')->hourly();
     }
+
 
     protected function commands(): void
     {
